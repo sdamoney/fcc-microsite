24e26110bb75814aaa120717ea6e0f01f76951c1
sdamoney <s.damani30@gmail.com>
2025-08-08 16:24:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// This uses the modern createRoot API for React 18+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
