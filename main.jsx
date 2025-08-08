41ecae008c5f476ddcc600fe4c2eec45586dcd9d
sdamoney <s.damani30@gmail.com>
2025-08-08 10:36:18 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FCCCardGame from "./FCCCardGame.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FCCCardGame />
+  </React.StrictMode>
+);
